5823a9d0b5383d0651b53265ef2a61dd82258ff0
Raphael <raphael@cloud13.de>
2016-06-20 14:16:00 +0200

    update scripts

--- docker/initDarknetDocker.sh
+++ docker/initDarknetDocker.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-docker build -t DockerDarknet .
+echo "Building Docker Image"
+docker build -t dockerdarknet .
+
+echo "Remove old Containers"
+docker rm -f darknet 2>/dev/null
+
+echo "Run new Container"
+nvidia-docker run -td -p 80:80 -v /opt/DockerDarknet:/opt/DockerDarknet --name "darknet" dockerdarknet
+echo "Clone repo inide Docker"
+nvidia-docker exec darknet /bin/sh -c "cd /opt/;mv /home/DockerDarknet/* /opt/DockerDarknet"
+
+echo "Installation Complete! Now use the scripts!"
 
