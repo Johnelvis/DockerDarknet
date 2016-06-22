3ceaa2f17d3f5f3c3bd4b1b21eea72f616e7dae2
Your Name <you@example.com>
2016-06-22 03:26:23 +0200

    make runnable

--- docker/initDarknetDocker.sh
+++ docker/initDarknetDocker.sh
@@ -8,7 +8,7 @@ docker rm -f darknet 2>/dev/null
 echo "Run new Container"
 nvidia-docker run -td -p 80:80 -v /opt/DockerDarknet:/opt/DockerDarknet --name "darknet" dockerdarknet
 echo "Clone repo inide Docker"
-nvidia-docker exec darknet /bin/sh -c "cp -r /home/DockerDarknet/* /opt/DockerDarknet;rm -r DockerDarknet;cd /opt/DockerDarknet;make"
+nvidia-docker exec darknet /bin/sh -c "cp -r /home/DockerDarknet /opt;rm -r DockerDarknet;cd /opt/DockerDarknet;git pull;make"
 
 echo "Installation Complete! Now use the scripts!"
 
