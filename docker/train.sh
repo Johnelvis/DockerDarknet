5cc089b8ef9602a0a01612016dd6037f1796f654
Raphael <raphael@cloud13.de>
2016-06-28 15:13:19 +0200

    rename edit and delete some files

--- docker/train.sh
+++ docker/train.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "-TRAIN DARKNET-"
 CONFIGFILE=$1
+shift;
+WIGHTFILE=$1
 eval `docker exec -i -t darknet ps aux | grep server | grep -v server | awk '{print "darknet exec -i -t darknet kill " $2}'`
 docker exec darknet /bin/sh -c "cd /opt/DockerDarknet/analyze;node server.js &"
-docker exec darknet /bin/sh -c "cd /opt/DockerDarknet;./darknet yolo train $CONFIGFILE"
+docker exec darknet /bin/sh -c "cd /opt/DockerDarknet;./darknet yolo train $CONFIGFILE $WIGHTFILE"
