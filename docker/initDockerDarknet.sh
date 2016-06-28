79b48f8c75a03c5826fb2d0dbf2d754d4848c711
Raphael <raphael@cloud13.de>
2016-06-28 22:34:35 +0200

    update initDockerDarknet to use dockerhub

--- docker/initDockerDarknet.sh
+++ docker/initDockerDarknet.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "Building Docker Image"
-docker build -t dockerdarknet .
+#echo "Building own Docker Image" #change the run line 9 as well!
+#docker build -t dockerdarknet .
 
 echo "Remove old Containers"
 docker rm -f darknet 2>/dev/null
 
 echo "Run new Container"
-nvidia-docker run -td -p 80:80 -v /opt/DockerDarknet:/opt/DockerDarknet --name "darknet" dockerdarknet
+nvidia-docker run -td -p 80:80 -v /opt/DockerDarknet:/opt/DockerDarknet --name "darknet" rofl256/dockerdarknet
 echo "Clone repo inide Docker"
 nvidia-docker exec darknet /bin/sh -c "cp -r /home/DockerDarknet /opt;cd /opt/DockerDarknet;git pull;make"
 ./start.sh
