cddd57ed1d2db754e6051008e02127259309ac7e
Raphael <raphael@cloud13.de>
2016-06-28 16:06:10 +0200

    update scripts

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo "Starting Darknet Container..."
 nvidia-docker start darknet
-echo "Start analyze tool"
-nvidia-docker exec darknet /bin/sh -c "cd /opt/DockerDarknet/analyze;node server.js &"
 echo "Darknet Container is ready to use!"
